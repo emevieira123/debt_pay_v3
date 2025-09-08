c6c07e0826117255cd1257549e08449402128f3d
Emerson Vieira <emerson.chaves@nuvem.net>
2025-09-08 13:30:53 -0400

    chore: adiciona e configura o axios e react-query

---
+++ src/infra/env.ts
@@ -0,0 +1,1 @@
+export const __API_BASE_URL__ = import.meta.env.VITE_REACT_APP_API_URL;
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Toaster } from "@/components/ui/sonner"
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './infra/queryClient.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-    <Toaster />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+      <Toaster />
+    </StrictMode>
+  </QueryClientProvider>,
 );
